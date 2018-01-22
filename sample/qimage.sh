7fd73835345dbcba39c271853d9de5cc628abd87
epaell <elenc@legolas.home>
2018-01-22 16:48:11 +1100

    Removed deprecated versions.

--- sample/qimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH --nodes=25
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=1:00:00
-#SBATCH --partition=gpuq
-#SBATCH --account=mwasci
-#SBATCH --export=NONE
-rm image.log
-rtsprocess.py image.in
